ff4cadc35ead3e5e8dc57e722b4d1d6782b1fc0a
mardam <nullachtfuffzn@arcor.de>
2017-04-04 16:59:08 +0200

    print confusion matrix

--- lstm.py
+++ lstm.py
@@ -2,8 +2,10 @@ import numpy
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import SimpleRNN
+from keras.layers import LSTM
 from keras.utils import np_utils
 from preprocess_data import *
+from sklearn.metrics import confusion_matrix, classification_report
 
 rows = parseCsvFile()
 maxLength = getHighestColumnNumber(rows)
@@ -66,12 +68,20 @@ tX = numpy.reshape(testX, (len(testX), 1, number_of_features))
 ty = np_utils.to_categorical(testY)
 
 model = Sequential()
-model.add(SimpleRNN(32, input_shape = (X.shape[1], X.shape[2])))
+model.add(SimpleRNN(40, input_shape = (X.shape[1], X.shape[2])))
 model.add(Dense(y.shape[1], activation = 'softmax'))
 model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics=['accuracy'])
-model.fit(X, y, epochs=200, batch_size = batch_size, verbose = 2, shuffle = False)
+model.fit(X, y, epochs=3, batch_size = batch_size, verbose = 2, shuffle = False)
 
 
 
-scores = model.evaluate(tX, ty, verbose = 0)
+scores = model.evaluate(tX, ty, verbose = 0, batch_size = batch_size)
 print("Model Accuracy: %.2f%%" % (scores[1]*100))
+
+ypred = numpy.argmax(model.predict(tX), axis = 1)
+print("----------------------------------")
+print("Confusion matrix:")
+print(confusion_matrix(numpy.argmax(ty, axis = 1), ypred))
+print("\n\n-----------------------------------")
+print("Metics:")
+print(classification_report(numpy.argmax(ty, axis = 1), ypred))
