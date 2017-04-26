fa4ceac2e5aaa375ee5657103a1f15af0e116975
mardam <nullachtfuffzn@arcor.de>
2017-04-26 21:42:51 +0200

    implement feature selection tree based

--- lstm.py
+++ lstm.py
@@ -7,29 +7,30 @@ from keras.layers import Dropout
 from preprocess_data import *
 import random
 from print_results import *
+from itertools import compress
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
 
 rows = parseCsvFile()
 maxLength = getHighestColumnNumber(rows)
 print("Length of longest row" + str(maxLength))
 print("Full cell features:" + str(rows[0][0].features) + "; length = " + str(len(rows[0][0].features)))
 
-paddedRows = padRows(rows, maxLength)
+normalizedRows = padRows(rows, maxLength)
 
-normalizedRows = normalizeData(paddedRows)
+print("rows_padded")
+del rows
 
-#for row in normalizedRows:
-#    for cell in row:
-#        cell.features = cell.features[1:4]
+normalizedRows = normalizeData(normalizedRows)
+
+print("rows_normalized")
 
 number_of_features = len(normalizedRows[0][0].features)
 print("Number of features: " + str(number_of_features))
 
 batch_size = len(normalizedRows[0])
 
-trainX = []
-trainY = []
-testX = []
-testY = []
+
 
 saveData(normalizedRows)
 
@@ -71,40 +72,68 @@ print("Number of Enron Files: " + str(len(enron_files)))
 print("Number of Fuse Files: " + str(len(fuse_files)))
 print("Number of Euses Files: " + str(len(euses_files)))
 
-random.seed(5)
-
-test_set = set(random.sample(enron_files, round(len(enron_files) / 3))).union(set(random.sample(fuse_files, round(len(fuse_files) / 3)))).union(set(random.sample(euses_files, round(len(euses_files) / 3))))
-
-print("Length of Test set: " + str(len(test_set)))
+X = []
+Y = []
 
 for row in normalizedRows:
     for cell in row:
-        if cell.file in test_set:
-            testX.append(cell.features)
-            testY.append(classLabelToNumber(cell.label))
-        else:
-            trainX.append(cell.features)
-            trainY.append(classLabelToNumber(cell.label))
+        X.append(cell.features)
+        Y.append(classLabelToNumber(cell.label))
 
 numpy.random.seed(7)
 
 
-X = numpy.reshape(trainX, (len(trainX), 1, number_of_features))
-y = np_utils.to_categorical(trainY)
+clf = ExtraTreesClassifier()
+clf = clf.fit(X, Y)
 
-tX = numpy.reshape(testX, (len(testX), 1, number_of_features))
-ty = np_utils.to_categorical(testY)
+model = SelectFromModel(clf, prefit = True)
+X = model.transform(X)
 
+printFeatureWeights(clf.feature_importances_, model.get_support(), "features_selection_tree_based.txt")
 
+print("feature selected")
 
-epoch_numbers = [1,3,5,10,20,30,50,100,200,300,400,500]
-for epoch_number in epoch_numbers:
-    model = Sequential()
-    model.add(SimpleRNN(40, input_shape = (X.shape[1], X.shape[2])))
-    model.add(Dropout(0.2))
-    model.add(Dense(y.shape[1], activation = 'softmax'))
-    model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics=['accuracy'])
-    model.fit(X, y, epochs=epoch_number, batch_size = batch_size, verbose = 2, shuffle = False)
+for row in normalizedRows:
+    for cell in row:
+        cell.features = list(compress(cell.features, model.get_support()))
 
-    printEvaluation("LSTM_Dropout" + str(epoch_number) + ".txt", model, X, y, tX, ty, batch_size)    
+print("features filtered")
+        
+seeds = [7,15,37]
+epoch_numbers = [10,20,30,50,100]
+for seed in seeds:
+    numpy.random.seed(seed)
+
+    random.seed(seed)
+    test_set = set(random.sample(enron_files, round(len(enron_files) / 3))).union(set(random.sample(fuse_files, round(len(fuse_files) / 3)))).union(set(random.sample(euses_files, round(len(euses_files) / 3))))
+    print("Length of Test set: " + str(len(test_set)))
+
+    trainX = []
+    trainY = []
+    testX = []
+    testY = []
+    
+    for row in normalizedRows:
+        for cell in row:
+            if cell.file in test_set:
+                testX.append(cell.features)
+                testY.append(classLabelToNumber(cell.label))
+            else:
+                trainX.append(cell.features)
+                trainY.append(classLabelToNumber(cell.label))
+
+    X = numpy.reshape(trainX, (len(trainX), 1, number_of_features))
+    y = np_utils.to_categorical(trainY)
+
+    tX = numpy.reshape(testX, (len(testX), 1, number_of_features))
+    ty = np_utils.to_categorical(testY)
+    for epoch_number in epoch_numbers:
+        model = Sequential()
+        model.add(LSTM(100, input_shape = (X.shape[1], X.shape[2])))
+        model.add(Dropout(0.2))
+        model.add(Dense(y.shape[1], activation = 'softmax'))
+        model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics=['accuracy'])
+        model.fit(X, y, epochs=epoch_number, batch_size = batch_size, verbose = 2, shuffle = False)
+
+        printEvaluation("LSTM_Dropout_tree_selection_epochs" + str(epoch_number) + "_seed" + str(seed) + ".txt", model, X, y, tX, ty, batch_size)    
 
